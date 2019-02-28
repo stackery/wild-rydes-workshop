5496cfce94b42e9b0bf406fd3227a3fc6602dace
Chase Douglas <chase@stackery.io>
2019-02-28 10:30:48 -0800

    Request unicorns from the unicorn stable

--- src/requestUnicorn/index.js
+++ src/requestUnicorn/index.js
@@ -1,4 +1,5 @@
 const randomBytes = require('crypto').randomBytes;
+const get = require('https').get;
 
 const AWS = require('aws-sdk');
 
@@ -22,38 +23,35 @@ const fleet = [
     },
 ];
 
-exports.handler = (event, context, callback) => {
-    if (!event.requestContext.authorizer) {
-      errorResponse('Authorization not configured', context.awsRequestId, callback);
-      return;
-    }
+exports.handler = async (event, context) => {
+    try {
+        if (!event.requestContext.authorizer) {
+          return errorResponse('Authorization not configured', context.awsRequestId);
+        }
 
-    const rideId = toUrlString(randomBytes(16));
-    console.log('Received event (', rideId, '): ', event);
+        const rideId = toUrlString(randomBytes(16));
+        console.log('Received event (', rideId, '): ', event);
 
-    // Because we're using a Cognito User Pools authorizer, all of the claims
-    // included in the authentication token are provided in the request context.
-    // This includes the username as well as other attributes.
-    const username = event.requestContext.authorizer.claims['cognito:username'];
+        // Because we're using a Cognito User Pools authorizer, all of the claims
+        // included in the authentication token are provided in the request context.
+        // This includes the username as well as other attributes.
+        const username = event.requestContext.authorizer.claims['cognito:username'];
 
-    // The body field of the event in a proxy integration is a raw string.
-    // In order to extract meaningful values, we need to first parse this string
-    // into an object. A more robust implementation might inspect the Content-Type
-    // header first and use a different parsing strategy based on that value.
-    const requestBody = JSON.parse(event.body);
+        // The body field of the event in a proxy integration is a raw string.
+        // In order to extract meaningful values, we need to first parse this string
+        // into an object. A more robust implementation might inspect the Content-Type
+        // header first and use a different parsing strategy based on that value.
+        const requestBody = JSON.parse(event.body);
 
-    const pickupLocation = requestBody.PickupLocation;
+        const pickupLocation = requestBody.PickupLocation;
 
-    const unicorn = findUnicorn(pickupLocation);
+        const unicorn = await findUnicorn(pickupLocation);
 
-    recordRide(rideId, username, unicorn).then(() => {
-        // You can use the callback function to provide a return value from your Node.js
-        // Lambda functions. The first parameter is used for failed invocations. The
-        // second parameter specifies the result data of the invocation.
+        await recordRide(rideId, username, unicorn);
 
         // Because this Lambda function is called by an API Gateway proxy integration
         // the result object must use the following structure.
-        callback(null, {
+        return {
             statusCode: 201,
             body: JSON.stringify({
                 RideId: rideId,
@@ -65,24 +63,72 @@ exports.handler = (event, context, callback) => {
             headers: {
                 'Access-Control-Allow-Origin': '*',
             },
-        });
-    }).catch((err) => {
+        };
+    } catch (err) {
         console.error(err);
 
         // If there is an error during processing, catch it and return
         // from the Lambda function successfully. Specify a 500 HTTP status
         // code and provide an error message in the body. This will provide a
         // more meaningful error response to the end client.
-        errorResponse(err.message, context.awsRequestId, callback)
-    });
+        return errorResponse(err.message, context.awsRequestId);
+    };
 };
+
+// Get the unicorn stable API key from AWS Secrets Manager. Cache the value
+// in `_apiKey` for subsequent invocations.
+let _apiKey;
+async function getUnicornStableApiKey() {
+    if (!_apiKey) {
+        const secretsManager = new AWS.SecretsManager();
+
+        // SECRETS_NAMESPACE is the namespace for this Stackery environment.
+        // This function only has read permissions to secrets underneath this
+        // namespace.
+        const secretId = process.env.SECRETS_NAMESPACE + 'unicornStableApiKey';
+
+        // Retrieve the secret value and store it in `_apiKey`
+        ({ SecretString: _apiKey } = await secretsManager.getSecretValue({
+            SecretId: secretId
+        }).promise());
+    }
+
+    return _apiKey;
+}
 
 // This is where you would implement logic to find the optimal unicorn for
 // this ride (possibly invoking another Lambda function as a microservice.)
 // For simplicity, we'll just pick a unicorn at random.
-function findUnicorn(pickupLocation) {
+async function findUnicorn(pickupLocation) {
     console.log('Finding unicorn for ', pickupLocation.Latitude, ', ', pickupLocation.Longitude);
-    return fleet[Math.floor(Math.random() * fleet.length)];
+
+    // Retrieve unicorn stable API key from AWS Secrets Manager
+    const apiKey = await getUnicornStableApiKey();
+
+    const url = `${process.env.UNICORN_STABLE_API}/unicorn`;
+
+    console.log(`Making request to url ${url}`);
+
+    const options = {
+        hostname: process.env.UNICORN_STABLE_API,
+        path: '/unicorn',
+        headers: {
+            'x-api-key': apiKey
+        }
+    };
+
+    return new Promise((resolve, reject) => {
+        https.get(options, res => {
+            res.setEncoding('utf8');
+            let body = '';
+
+            res.on('data', data => body += data);
+
+            res.on('end', () => resolve(JSON.parse(body)));
+
+            res.on('error', err => reject(err));
+        });
+    });
 }
 
 function recordRide(rideId, username, unicorn) {
@@ -105,8 +151,8 @@ function toUrlString(buffer) {
         .replace(/=/g, '');
 }
 
-function errorResponse(errorMessage, awsRequestId, callback) {
-  callback(null, {
+function errorResponse(errorMessage, awsRequestId) {
+  return {
     statusCode: 500,
     body: JSON.stringify({
       Error: errorMessage,
@@ -115,5 +161,5 @@ function errorResponse(errorMessage, awsRequestId, callback) {
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
-  });
+  };
 }
