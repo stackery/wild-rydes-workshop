1ad7edb19c2f87cf599a9e920de80c9ed61e728b
Chase Douglas <chase@stackery.io>
2019-04-22 15:52:49 -0700

    Initial function scaffolding

--- src/populate_frontend_content/handler.py
+++ src/populate_frontend_content/handler.py
@@ -1,6 +1,17 @@
 import json
+
+import cfnresponse
+
 def handler(event, context):
     # Log the event argument for debugging and for use in local development.
     print(json.dumps(event, indent=2))
+
+    try:
+        cfnresponse.send(event, context, cfnresponse.SUCCESS, {}, 'PopulateFrontendContent')
+
+        print('Succeeded in uploading frontend content!')
+    except Exception as err:
+        print(f'Failed to upload frontend content: {err}')
+        cfnresponse.send(event, context, cfnresponse.FAILED, {}, 'PopulateFrontendContent')
 
     return {}
