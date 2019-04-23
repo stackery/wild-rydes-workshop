cd597ccac2dea3777c1fd669e6030aa61e6e5846
Chase Douglas <chase@stackery.io>
2019-04-22 17:20:39 -0700

    Upload static content

--- src/populate_frontend_content/handler.py
+++ src/populate_frontend_content/handler.py
@@ -1,5 +1,8 @@
 import json
+import mimetypes
+import os
 
+import boto3
 import cfnresponse
 
 def handler(event, context):
@@ -7,6 +10,8 @@ def handler(event, context):
     print(json.dumps(event))
 
     try:
+        upload_static_content()
+
         cfnresponse.send(event, context, cfnresponse.SUCCESS, {}, 'PopulateFrontendContent')
 
         print('Succeeded in uploading frontend content!')
@@ -15,3 +20,19 @@ def handler(event, context):
         cfnresponse.send(event, context, cfnresponse.FAILED, {}, 'PopulateFrontendContent')
 
     return {}
+
+def upload_static_content():
+    session = boto3.Session()
+    s3 = session.resource('s3')
+    bucket = s3.Bucket(os.environ['BUCKET_NAME'])
+
+    for subdir, _, files in os.walk('static'):
+        for file in files:
+            local_path = os.path.join(subdir, file)
+            key = os.path.relpath(local_path, 'static')
+            mimetype = mimetypes.guess_type(key)[0] or 'application/octet-stream'
+
+            print(f'Uploading {key} as {mimetype}')
+
+            with open(local_path, 'rb') as data:
+                bucket.put_object(Key=key, Body=data, ContentType=mimetype, ACL='public-read')
