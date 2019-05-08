950c2ad60d83fbc7f1cca6b792904dade255554e
gal bashan <gal@epsagon.com>
2019-05-08 15:02:55 +0300

    [monitoring] Switching to sendgrid

--- src/MailObject/index.js
+++ src/MailObject/index.js
@@ -1,7 +1,7 @@
 const AWS = require('aws-sdk');
+const sgMail = require('@sendgrid/mail');
 
 const s3 = new AWS.S3();
-const ses = new AWS.SES();
 
 exports.handler = async (event, context) => {
   // Log the event argument for debugging and for use in local development.
@@ -28,28 +28,39 @@ async function mailObject(s3Obj) {
   let objBody = await s3.getObject(params).promise().Body;
 
 
-  let sendParams = {
-    Destination: {
-      ToAddresses: [
-        objTags.email,
-      ],
-    },
-    Message: {
-      Body: {
-        Text: {
-          Charset: "UTF-8",
-          Data: objBody,
-        },
-      },
-      Subject: {
-        Charset: "UTF-8",
-        Data: objTags.subject,
-      },
-    },
-    Source: "info@wildrydes.com"
-  }
+  // let sendParams = {
+  //   Destination: {
+  //     ToAddresses: [
+  //       objTags.email,
+  //     ],
+  //   },
+  //   Message: {
+  //     Body: {
+  //       Text: {
+  //         Charset: "UTF-8",
+  //         Data: objBody,
+  //       },
+  //     },
+  //     Subject: {
+  //       Charset: "UTF-8",
+  //       Data: objTags.subject,
+  //     },
+  //   },
+  //   Source: "info@wildrydes.com"
+  // }
+  const msg = {
+    to: objTags.email,
+    from: 'info@wildrydes.com',
+    subject: objTags.subject,
+    text: objBody,
+    mail_settings: {
+      sandbox_mode: {
+        enable: true
+      }
+    }
+  };
 
-  await ses.sendEmail(sendParams).promise();
+  await sgMail.send(msg);
 }
 
 
